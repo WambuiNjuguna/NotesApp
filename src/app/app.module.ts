555a37ba3b21a94b7e1e8d6c6af47d1c3e1e42cf
unknown <marynjuguna388@gmail.com>
2024-10-24 12:24:29 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment.development';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: "AIzaSyAerPFYq6J--EjO4PgGdgZqYpLD6OMnudQ",
+      authDomain: "notesapp-f8f1a.firebaseapp.com",
+      projectId: "notesapp-f8f1a",
+      storageBucket: "notesapp-f8f1a.appspot.com",
+      messagingSenderId: "1028501264139",
+      appId: "1:1028501264139:web:b426322ace2aa56417340e"
+    }
+  };
+  
